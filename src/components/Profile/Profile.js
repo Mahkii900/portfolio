bebf89370cbd82dbe30efb5d6e74ffc14676defa
Mahkii900 <col.graff.battleschool@gmail.com>
2020-01-09 18:20:42 -0700

    working on profile image

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './Profile.css'
 
 export default class Profile extends Component {
     render() {
@@ -6,6 +7,8 @@ export default class Profile extends Component {
             <div>
                 <div>
                     <h1>Daniel Lawyer</h1>
+                    <img src="IMG_1715.JPG" alt='myself' height="50px"></img>
+                    {/* <div className="yoMom"></div> */}
                 </div>
                 <div>
                     <h2>About</h2>
