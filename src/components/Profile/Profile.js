a01cd74a5892c35438ac383432b4e34f354b584b
Mahkii900 <col.graff.battleschool@gmail.com>
2020-01-14 12:34:30 -0700

    added items to profile, fixed picture display and scrolling issues

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,14 +7,16 @@ export default class Profile extends Component {
             <div>
                 <div>
                     <h1>Daniel Lawyer</h1>
-                    <img src="IMG_1715.JPG" alt='myself' height="50px"></img>
-                    {/* <div className="yoMom"></div> */}
+                    <a data-flickr-embed="true" href="https://www.flickr.com/photos/dclawyer/49358897223/" title="Daniel Lawyer"><img src="https://live.staticflickr.com/65535/49358897223_b9a3b519b7_4k.jpg" width="400" alt="Myself"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
                 </div>
                 <div>
                     <h2>About</h2>
                     <div>
                         <p>
-                            Lorem Ipsum
+                            I am a full stack web developer from Utah.
+                            I graduated from DevMountain's web development course in September 2019.
+                            I have an interest in and a passion for math.
+                            I also enjoy coding, anime, manga, board games, strategy games, and trying out new types of games.
                         </p>
                     </div>
                 </div>
@@ -22,7 +24,26 @@ export default class Profile extends Component {
                     <h2>Skills</h2>
                     <div>
                         <p>
-                            Lorem Ipsum
+                            I can use the following skills:
+                        </p>
+                        <p>Git</p>
+                        <p>Express</p>
+                        <p>Node</p>
+                        <p>Javascript</p>
+                        <p>HTML 5</p>
+                        <p>CSS 3</p>
+                        <p>D3</p>
+                        <p>NodeMailer</p>
+                        <p>C++</p>
+                        <p>React</p>
+                        <p>TI BASIC</p>
+                    </div>
+                </div>
+                <div>
+                    <h2>GitHub</h2>
+                    <div>
+                        <p>
+                            GitHub Page Link
                         </p>
                     </div>
                 </div>
