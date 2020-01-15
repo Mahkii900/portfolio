94dae5576c1daca5c3eee405fdbef41136f28311
Mahkii900 <col.graff.battleschool@gmail.com>
2020-01-15 16:19:23 -0700

    adding logos to skills

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -27,16 +27,16 @@ export default class Profile extends Component {
                             I can use the following skills:
                         </p>
                         <p>Git</p>
-                        <p>Express</p>
+                        <img src="https://git-scm.com/images/logos/downloads/Git-Icon-White.png" alt='git' height='100px' />
                         <p>Node</p>
+                        <img src='https://nodejs.org/static/images/logos/nodejs-new-pantone-white.svg' alt='node' height='100px' />
                         <p>Javascript</p>
                         <p>HTML 5</p>
                         <p>CSS 3</p>
                         <p>D3</p>
-                        <p>NodeMailer</p>
                         <p>C++</p>
                         <p>React</p>
-                        <p>TI BASIC</p>
+                        <img src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K' alt='react' height='100px' />
                     </div>
                 </div>
                 <div>
