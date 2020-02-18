de648a047982b079593b2cf2a80b9bdc8f3a7575
Mahkii900 <col.graff.battleschool@gmail.com>
2020-02-18 13:46:51 -0700

    added styling to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,22 +1,32 @@
 import React, {Component} from 'react'
+import './Contact.css'
 
 export default class Contact extends Component {
     render() {
         return(
-            <div>
+            <div className='contact'>
                 <div>
                     <h1>Contact Me</h1>
                 </div>
                 <div>
+                    <p>
+                        Write a message to me! Please leave contact information as well if you want me to respond back.
+                    </p>
+                </div>
+                <div className='contact-content'>
                     <div>
-                        <p>
-                            Write a message to me! Please leave contact information as well if you want me to respond back.
-                        </p>
+                        Name:
+                        <input></input>
                     </div>
                     <div>
+                        Email:
+                        <input></input>
+                    </div>
+                    <div className='message-area'>
+                        Message
                         <textarea></textarea>
                     </div>
-                    <div>
+                    <div className='submit-area'>
                         <button>Submit</button>
                     </div>
                 </div>
