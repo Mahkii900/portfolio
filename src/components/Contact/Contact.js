e93e9bec0a99aa3b9acc52a97374afcecff6750d
Mahkii900 <col.graff.battleschool@gmail.com>
2020-02-18 15:46:33 -0700

    added message functionality to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,42 @@
 import React, {Component} from 'react'
 import './Contact.css'
+import axios from 'axios'
 
 export default class Contact extends Component {
+    state = {
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    }
+
+    handleNameChange(e) {
+        this.setState({name: e.target.value})
+    }
+
+    handleEmailChange(e) {
+        this.setState({email: e.target.value})
+    }
+
+    handleSubjectChange(e) {
+        this.setState({subject: e.target.value})
+    }
+
+    handleMessageChange(e) {
+        this.setState({message: e.target.value})
+    }
+
+    submitMessage() {
+        axios.post('/messages', {
+            name: this.state.name,
+            email: this.state.email,
+            subject: this.state.subject,
+            message: this.state.message
+        })
+        .catch(err => alert("Failed to send email"))
+        this.setState({name: '', email: '', subject: '', message: ''})
+    }
+
     render() {
         return(
             <div className='contact'>
@@ -16,22 +51,22 @@ export default class Contact extends Component {
                 <div className='contact-content'>
                     <div>
                         Name:
-                        <input></input>
+                        <input value={this.state.name} onChange={(e) => this.handleNameChange(e)}/>
                     </div>
                     <div>
                         Email:
-                        <input></input>
+                        <input value={this.state.email} onChange={(e) => this.handleEmailChange(e)}/>
                     </div>
                     <div>
                         Subject:
-                        <input></input>
+                        <input value={this.state.subject} onChange={(e) => this.handleSubjectChange(e)} />
                     </div>
                     <div className='message-area'>
                         Message
-                        <textarea></textarea>
+                        <textarea value={this.state.message} onChange={(e) => this.handleMessageChange(e)} ></textarea>
                     </div>
                     <div className='submit-area'>
-                        <button>Submit</button>
+                        <button onClick={() => this.submitMessage()}>Submit</button>
                     </div>
                 </div>
             </div>
